bc0570bf69b37861068ddcef486084744bcc651e
stef-the <stefthedoggo4@gmail.com>
2022-01-27 09:39:31 -0800

    push

--- api/src/server.js
+++ api/src/server.js
@@ -1,18 +1,15 @@
-const express = require("express");
+const app = require("express")();
 const bodyParser = require("body-parser");
 
 /*const { createServer } = require('http')
 const server = createServer(() => {})
 server.listen(4000)*/
 
-const app = express();
-const port = 4000;
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('trust proxy', true)
 
 require("./routes/_import")(app);
 
-app.listen(port, () => {
-	console.log("API is live on port " + port);
+app.listen(4000, () => {
+	console.log("API is live on port " + 4000);
 });
