2533c8052b7418f3cd8614bb38119c349d6f81b2
stef-the <stefthedoggo4@gmail.com>
2022-01-28 08:30:01 -0800

    Update login.js

--- api/pages/api/login.js
+++ api/pages/api/login.js
@@ -1,3 +1,5 @@
+import { addMinutes } from "date-fns";
+
 export default function handler(req, res) {
 	const users = require("../../data/users.json");
 	var output = {};
@@ -6,25 +8,19 @@ export default function handler(req, res) {
 	for (const rawquery of req.url.split("?")[1].split("&")) {
 		query[rawquery.split("=")[0]] = rawquery.split("=")[1];
 	}
+
 	console.log(query);
 	console.log(users)
 
 	output.user = { exists: false, pass: false };
 	output.login = false;
 
-	for (const user of users) {
-		if (user.id == query.id) {
-			output.user.exists = true;
-			if (user.pass == query.pass) {
-				output.user.pass = true;
-				output.login = true;
-			}
-		}
+	if (users.find(user => user.id == query.id && user.pass == query.pass)) {
+		output.login = true;
 	}
 
 	res.status(200).json({
 		query: query,
 		login: output.login,
-		user: output.user,
 	});
 }
